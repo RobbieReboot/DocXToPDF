9c2661436d91ca0009a9fd5699111edb3a2786db
Rob Hill <rob.hill@3squared.com>
2019-02-26 10:06:39 +0000

    updates nuspec name

--- powershell/packandpush.ps1
+++ powershell/packandpush.ps1
@@ -1,12 +1,9 @@
 ﻿param (
-    [string]$project = ".\DocXToPdf\DocXToPdf"
+    [string]$project = ".\DocXToPdf.Core\DocXToPdf.Core.csproj"
     )
 
 #Build & pack
-& "nuget.exe" pack -Build -properties Configuration=Release $project.csproj
-& "nuget.exe" push *.nupkg -apikey woAsVXBI8v8W9UgIiNOr -Source http://3sq-ih-proget:8624/nuget/3SquaredCommonNuget/
+& "nuget.exe" pack -Build -properties Configuration=Release $project
+#& "nuget.exe" push *.nupkg -apikey woAsVXBI8v8W9UgIiNOr -Source http://3sq-ih-proget:8624/nuget/3SquaredCommonNuget/
 
-
-
-
-
+#nuget.exe pack -Build -properties Configuration=Release .\DocXToPdf.Core\DocXToPdf.Core.nusp
