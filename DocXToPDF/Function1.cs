1701a255cb2c49ce1efcde82949efcc5c8f64580
Rob Hill <rob.hill@3squared.com>
2019-03-04 09:58:56 +0000

    removes parameters from Azure Function
    adds new Azure publish profile

--- DocXToPDF/Function1.cs
+++ DocXToPDF/Function1.cs
@@ -1,4 +1,5 @@
 
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Xml;
@@ -16,16 +17,15 @@ namespace DocXToPDF
     public static class DocXtoPdfAzureFunction
     {
         [FunctionName("docx2pdf")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous,"get", "post", Route = null)]HttpRequest req, TraceWriter log, ExecutionContext context)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous,"get", "post", Route = null)]HttpRequest req, 
+            TraceWriter log,
+            ExecutionContext context)
         {
             log.Info("DocX2Pdf function request.");
-            //Stopwatch sw = new Stopwatch();
-            string fileName = req.Query["name"];
 
             var memoryStream = new MemoryStream();
             req.Body.CopyTo(memoryStream);
 
-            XDocument xdoc = null;
             PdfDocument pdfDoc;
 
             if (req.Body.Length==0)
@@ -36,12 +36,8 @@ namespace DocXToPDF
             {
                 pdfDoc = PdfDocument.FromDocX(memoryStream);
             }
-
             var str = pdfDoc.GetBytes(new MemoryStream());
-            var result = new FileContentResult(str, "application/pdf");
-            if (!string.IsNullOrEmpty(fileName))
-                fileName = Path.ChangeExtension(fileName, "pdf");
-            result.FileDownloadName = fileName ?? "download.pdf";
+            var result = new FileContentResult(str, "application/pdf") {FileDownloadName = "download.pdf"};
             return result;
         }
     }
